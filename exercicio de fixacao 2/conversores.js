3679abeddf6ea1fc4904545428fa3830ac7d1cb6
Alexandre-mamede <amamedepatrocinio@gmail.com>
2026-09-21 09:23:12 -0300

    isso ai

---
+++ exercicio de fixacao 2/conversores.js
@@ -0,0 +1,14 @@
+class Conversor {
+    celsiusParaFahrenheit(celsius) {
+        return F = (9 / 5) * C + 32
+    }
+    quilometrosParaMilhas(km){
+        return milhas = km * 0.621371
+    }
+    minutosParaHoras(minutos){
+        return horas = minutos / 60
+    }
+   
+}
+
+module.export = Conversor;
---
+++ exercicio de fixacao 2/main.js
@@ -0,0 +1,5 @@
+const Conversor = require('./Conversores.js')
+
+console.log(conversor.celsiusParaFahrenheit(25));
+console.log(conversor.quilometrosParaMilhas(10));
+console.log(conversor.minutosParaHoras(150));
